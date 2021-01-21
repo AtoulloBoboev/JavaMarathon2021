e07773041f2b28a2d3b7d470b7df6ad6c0b2b1fc
Atoullo Boboev <atoullo.boboev@gmail.com>
2021-01-21 16:01:47 -0500

    Added day-1 solutions.

--- src/main/java/day1/Task6.java
+++ src/main/java/day1/Task6.java
@@ -1,7 +1,12 @@
 package day1;
 
+import java.util.Scanner;
+
 public class Task6 {
     public static void main(String[] args) {
-
+        int k = 5;
+        for (int i = 1; i <= 9; i++) {
+            System.out.println(k + " x " + i + " = " + (k * i));
+        }
     }
 }
