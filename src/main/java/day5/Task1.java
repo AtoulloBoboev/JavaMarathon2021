0f9d3daa80ddc5b2b1968d8c5c42c86e90db4954
Atoullo Boboev <atoullo.boboev@gmail.com>
2021-01-28 16:55:01 -0500

    Added day-5 solutions.

--- src/main/java/day5/Task1.java
+++ src/main/java/day5/Task1.java
@@ -2,6 +2,11 @@ package day5;
 
 public class Task1 {
     public static void main(String[] args) {
+        Car car = new Car();
+        car.setColor("black");
+        car.setModel("Tesla");
+        car.setYear(2020);
 
+        System.out.println("Our car: " + car.getYear() + " year " + car.getColor() + " color " + car.getModel() + " model");
     }
 }
--- src/main/java/day5/Task2.java
+++ src/main/java/day5/Task2.java
@@ -3,6 +3,10 @@ package day5;
 
 public class Task2 {
     public static void main(String[] args) {
+        Motorbike bike = new Motorbike(2020, "black", "kawasaki");
 
+        System.out.println(bike.getModel() + "\n " +
+                bike.getColor() + " \n " +
+                bike.getYear());
     }
 }
