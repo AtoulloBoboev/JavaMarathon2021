0e02a225b91c1e35444b2ff21fac79ba7f77b075
Atoullo Boboev <atoullo.boboev@gmail.com>
2021-02-19 16:29:28 -0500

    Added day-13 solutions.

--- src/main/java/day8/Task2.java
+++
@@ -1,9 +0,0 @@
-package day8;
-
-public class Task2 {
-    public static void main(String[] args) {
-    Airplane airplane = new Airplane("Boeing", 2018, 80, 50000);
-
-        System.out.println(airplane);
-    }
-}
